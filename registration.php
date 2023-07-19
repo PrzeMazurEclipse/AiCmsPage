20e7d93680765e5ca01d801a978cc391b8927b7e
Przemyslaw Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
2023-07-19 11:05:23 +0200

    Add initial files

---
+++ registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author Przemysław Mazurkiewicz <przemyslaw.mazurkiewicz@eclipsegroup.co.uk>
+ * @copyright 2023 Eclipse
+ * @since     1.0.0
+ */
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'Eclipse_CmsPageEdition',
+    __DIR__
+);
